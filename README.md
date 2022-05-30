f6542763b9c56ee4e16385059675a8d4b7fa5d73
Oleinikov, Bogdan Konstantinovich <boleinikov3@gatech.edu>
2022-05-30 17:55:03 +0900

    update fav icon and add css

--- README.md
+++ README.md
@@ -5,6 +5,13 @@ implemented: added basic skeleton for the resume
 
 to do: add more css :)
 
+# Version 2
+---
+
+implemented: Typography on Titles and Progress Bar for languages
+
+to do: add smaller fonts to descriptions and badges for certificates, update information, add drop downs and hides to resume
+
 
 
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
 import React, { FC } from 'react';
-import { Button, Row, Col, Divider } from 'antd';
+import { Button, Row, Col, Divider, Typography, Progress } from 'antd';
 import './App.css';
-
+const { Title, Text } = Typography;
 
 const App: FC = () => (
   <div className="App">
     <Row className="resume">
-      <Col span={16} >
-        <Row>
-          <Col className="name-box" span={12} >
-            <div className='first-name'>Bogdan K. </div>
-            <div className='last-name'>Oleinikov</div>
+      <Col span={16} className="left-column" >
+        <Row align='middle'>
+          <Col span={12} >
+            <Title level={1}>
+              <div className='first-name'>Bogdan K. </div>
+              <div className='last-name'>Oleinikov</div>
+            </Title>
           </Col>
           <Col span={12} >
-            <div>Contact</div>
+            <Title level={4} underline={true}>Contact</Title>
             <div>☏ +82 010-4478-6630</div>
             <div>✉ boleinikov@gmail.com</div>
           </Col>
         </Row>
         <div>
-          <div>Education</div>
+          <Title level={4} >Education</Title>
           <Divider />
           <div>
-            <div>Online Master of Computer Science <br />
+            <div>
+              <Title level={5} >Online Master of Computer Science </Title>
               Georgia Tech | 2020 - 2022 <br />
               ↪Expected graduation December 2022
             </div>
             <div>
-              Undergraduate Computer Science Student in College of <br />
-              Engineering and Applied Sciences <br />
+              <Title level={5}> Undergraduate Computer Science Student in College of
+                Engineering and Applied Sciences </Title>
               SUNY Korea University | 2015 - 2017 & 2018 - 2019 <br />
               ↪Cumulative GPA 3.49/4.00 <br />
               Stony Brook University | 2017 - 2018 <br />
@@ -36,62 +39,62 @@ const App: FC = () => (
           </div>
         </div>
         <div>
-          <div>Experience</div>
+          <Title level={4} >Experience</Title>
           <Divider />
           <div>
             <div>
-              Jinjoosoft Software Engineer<br />
+              <Title level={5}> Jinjoosoft Software Engineer</Title>
               Web/Mobile Frontend & Backend Developer | Oct 2019 - Oct 2020<br />
               • Use of Angular, React, and Vue libraries for web development.<br />
               • Use of React Native for mobile development.<br />
               • Use of Netcore, Docker, and Express for backend development.
             </div>
             <div>
-              BNR Technology Intern<br />
+              <Title level={5}>BNR Technology Intern</Title>
               Android Mobile Development &<br />
               Eng-Rus-Kor Translator Position | Jan 2019 - Mar 2019<br />
               • Use of Android Studios and Firebase for mobile app software development.<br />
               • Created written translations for English, Russian, and Korean language.
             </div>
             <div>
-              Family Library Project<br />
+              <Title level={5}>Family Library Project</Title>
               Personal Project | Spring 2022<br />
-              • Deployment of a family library database providing frontend React Typescript<br />
-              GUI and backend implementation for addition/modification of book records<br />
+              • Deployment of a family library database providing frontend React Typescript
+              GUI and backend implementation for addition/modification of book records
               using MongoDB.
             </div>
           </div>
         </div>
         <div>
-          <div>Certifications</div>
+          <Title level={4} >Certifications</Title>
           <Divider />
           <div>
             <div>
-              Oracle Certified Professional<br />
+              <Title level={5}>Oracle Certified Professional</Title>
               Java SE 8 Programmer | July 20, 2018<br />
-              • Verified understanding of Concurrency, Fork - Join Framework, Functional Interfaces,<br />
-              JDBC, Java Design Patterns, Java NIO.2 API, Localization, Regular Expressions,<br />
-              Parallel Streams and Stream API.<br />
+              • Verified understanding of Concurrency, Fork - Join Framework, Functional Interfaces,
+              JDBC, Java Design Patterns, Java NIO.2 API, Localization, Regular Expressions,
+              Parallel Streams and Stream API.
             </div>
             <div>
-              Oracle Certified Associate<br />
+              <Title level={5}>Oracle Certified Associate</Title>
               Java SE 8 Programmer | June 2, 2018<br />
-              • Verified understanding of Java, JDK 8, Java SE 8, Lambda Expressions, Java<br />
-              Application Development, and Java 8 Date & Time API.<br />
+              • Verified understanding of Java, JDK 8, Java SE 8, Lambda Expressions, Java
+              Application Development, and Java 8 Date & Time API.
             </div>
             <div>
-              Test of Proficiency in Korean<br />
+              <Title level={5}>Test of Proficiency in Korean</Title>
               TOPIK Level 6 | Oct 2021<br />
-              • Absolutely fluent in the Korean language for professional research or work, Capacity<br />
-              to understand and express oneself without problem, although without the full fluency of<br />
+              • Absolutely fluent in the Korean language for professional research or work, Capacity
+              to understand and express oneself without problem, although without the full fluency of
               a native speaker.
             </div>
           </div>
         </div>
       </Col>
       <Col className="right-column" span={8}  >
-        <div>Software Engineer</div>
-        <div>Relevent Coursework</div>
+        <Title level={2}  ><Text className="job-title" >Software Engineer</Text></Title>
+        <Title level={4} >Relevent Coursework</Title>
 
         <Divider />
         <div>
@@ -116,10 +119,10 @@ const App: FC = () => (
           Robotics<br />
           Visualization
         </div>
-        <div>Skills</div>
+        <Title level={4} >Skills</Title>
         <Divider />
 
-        <div>Computer</div>
+        <Title level={5} >Computer</Title>
         <div>
           Proficient in Java, Python, C#<br />
           Adept in Web Component<br />
@@ -130,11 +133,44 @@ const App: FC = () => (
           MongoDB, Network Sockets<br />
           Novice in Lisp, SML<br />
         </div>
-        <div>Languages</div>
+        <Title level={5} >Languages</Title>
         <div>
           English (Native/ US Citizen)<br />
+          <Progress
+            strokeColor={{
+              from: '#6FA8DC',
+              to: '#073763',
+            }}
+            percent={100}
+            showInfo={false}
+            status="active"
+            trailColor='transparent'
+            strokeWidth={15}
+          />
           Korean (Fluent/ 6 years+)<br />
+          <Progress
+            strokeColor={{
+              from: '#6FA8DC',
+              to: '#073763',
+            }}
+            percent={80}
+            showInfo={false}
+            status="active"
+            trailColor='transparent'
+            strokeWidth={15}
+          />
           Russian (Fluent/ Heritage Speaker)<br />
+          <Progress
+            strokeColor={{
+              from: '#6FA8DC',
+              to: '#073763',
+            }}
+            percent={80}
+            showInfo={false}
+            status="active"
+            trailColor='transparent'
+            strokeWidth={15}
+          />
         </div>
       </Col>
     </Row>
