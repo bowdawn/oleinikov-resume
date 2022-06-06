8db23c0ffd7a103b1d1b122532e12b69c63b42a0
Oleinikov, Bogdan Konstantinovich <boleinikov3@gatech.edu>
2022-06-06 16:28:56 +0900

    translated

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,107 @@
-import React, { FC } from 'react';
-import { Button, Row, Col, Divider, Typography, Progress } from 'antd';
+import React, { FC, useState } from 'react';
+import { Button, Row, Col, Divider, Typography, Progress, PageHeader, Switch } from 'antd';
 import './App.css';
+import { GlobalOutlined } from '@ant-design/icons';
 const { Title, Text } = Typography;
 
-const App: FC = () => (
-  <div className="App">
+
+
+
+interface resume {
+  first: string;
+  last: string;
+  contact: string;
+  education: string;
+  experience: string;
+  certifications: string;
+  software: string;
+  engineer: string;
+  releventcoursework: string;
+  skills: string;
+  computer: string;
+  languages: string;
+}
+
+const resume_en: resume =
+{
+  first: "Bogdan K.",
+  last: "Oleinikov",
+  contact: "Contact",
+  education: "Education",
+  experience: "Experience",
+  certifications: "Certifications",
+  software: "Software",
+  engineer: "Engineer",
+  releventcoursework: "Relevent Coursework",
+  skills: "Skills",
+  computer: "Computer",
+  languages: "Langauges"
+}
+
+const resume_kr: resume =
+{
+  first: "복단",
+  last: "오",
+  contact: "연락처",
+  education: "학력사항",
+  experience: "경력사항",
+  certifications: "자격사항",
+  software: "소프트웨어",
+  engineer: "엔지니어",
+  releventcoursework: "관련 전공수강 내역",
+  skills: "보유능력",
+  computer: "컴퓨터",
+  languages: "언어"
+}
+
+
+
+const App: FC = () => {
+  const [kr, setKr] = useState(false);
+  return (<div className="App">
     <Row className="resume">
+      <Col span={24}>
+        <PageHeader extra={<Switch checked={kr} onClick={() => setKr(!kr)} unCheckedChildren={<div><GlobalOutlined /> En/Kr</div>} checkedChildren={<div><GlobalOutlined /> 한/영</div>} />}></PageHeader>
+      </Col>
+
       <Col span={16} className="left-column" >
         <Row align='middle'>
           <Col span={12} >
-            <Title level={1}>
-              <div className='first-name'>Bogdan K. </div>
-              <div className='last-name'>Oleinikov</div>
-            </Title>
+            {!kr ?
+              <Title className="name" level={1}>
+                <div className='first-name'>{resume_en.first}</div>
+                <div className='last-name'>{resume_en.last}</div>
+              </Title>
+              :
+              <div className='name'>
+                <Title level={2}>
+                  <div className='first-name'>{resume_en.first}</div>
+                  <div className='last-name'>{resume_en.last}</div>
+                </Title>
+                <Text strong>(국문: {resume_kr.last  + resume_kr.first})</Text>
+              </div>
+            }
+
           </Col>
           <Col span={12} >
-            <Title level={4} underline={true}>Contact</Title>
+            <Title level={4} underline={true}>{!kr ? resume_en.contact : resume_kr.contact}</Title>
             <div>☏ +82 010-4478-6630</div>
             <div>✉ boleinikov@gmail.com</div>
           </Col>
         </Row>
         <div>
-          <Title level={4} >Education</Title>
+          <Title level={4} >{!kr ? resume_en.education : resume_kr.education}</Title>
           <Divider />
           <div>
             <div>
-              <Title level={5} >Master's Degree in Computer Science 
+              <Title level={5} >Master's Degree in Computer Science
               </Title>
               <div className='description-title'>Georgia Tech | 2020 - 2022 </div>
               <div className='description-detail'>↪Expected graduation December 2022</div>
             </div>
             <div>
               <Title level={5}>Bachelor's Degree in Computer Science
-                </Title>
+              </Title>
               <div className='description-title'>SUNY Korea University | 2015 - 2017 & 2018 - 2019</div>
               <div className='description-detail'>↪Cumulative GPA 3.49/4.00 </div >
               <div className='description-title'>Stony Brook University | 2017 - 2018 </div>
@@ -40,7 +109,7 @@ const App: FC = () => (
           </div>
         </div>
         <div>
-          <Title level={4} >Experience</Title>
+          <Title level={4} >{!kr ? resume_en.experience : resume_kr.experience}</Title>
           <Divider />
           <div>
             <div>
@@ -68,7 +137,7 @@ const App: FC = () => (
           </div>
         </div>
         <div>
-          <Title level={4} >Certifications</Title>
+          <Title level={4} >{!kr ? resume_en.certifications : resume_kr.certifications}</Title>
           <Divider />
           <div>
             <div>
@@ -95,8 +164,11 @@ const App: FC = () => (
         </div>
       </Col>
       <Col className="right-column" span={8}  >
-        <Title level={2}  ><Text className="job-title" >Software Engineer</Text></Title>
-        <Title level={4} >Relevent Coursework</Title>
+        <Title level={2}  >
+          <Text className="job-title" >{!kr ? resume_en.software : resume_kr.software}</Text><br />
+          <Text className="job-title" >{!kr ? resume_en.engineer : resume_kr.engineer}</Text>
+        </Title>
+        <Title level={4} >{!kr ? resume_en.releventcoursework : resume_kr.releventcoursework}</Title>
 
         <Divider />
         <div className='description-title'>
@@ -123,10 +195,10 @@ const App: FC = () => (
           Robotics<br />
           Visualization
         </div>
-        <Title level={4} >Skills</Title>
+        <Title level={4} >{!kr ? resume_en.skills : resume_kr.skills}</Title>
         <Divider />
 
-        <Title level={5} >Computer</Title>
+        <Title level={5} >{!kr ? resume_en.computer : resume_kr.computer}</Title>
         <div>
           Proficient in Java, Python, C#<br />
           Adept in Web Component<br />
@@ -137,7 +209,7 @@ const App: FC = () => (
           MongoDB, Network Sockets<br />
           Lisp, SML<br />
         </div>
-        <Title level={5} >Languages</Title>
+        <Title level={5} >{!kr ? resume_en.languages : resume_kr.languages}</Title>
         <div>
           English (Native/ US Citizen)<br />
           <Progress
@@ -180,6 +252,10 @@ const App: FC = () => (
     </Row>
 
   </div>
-);
+  )
+}
+
+
+
 
 export default App;
