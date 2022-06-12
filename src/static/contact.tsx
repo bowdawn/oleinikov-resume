d79e865a5ce7b3d5fb5cb36c118ebd523e0384d8
Oleinikov, Bogdan Konstantinovich <boleinikov3@gatech.edu>
2022-06-12 16:24:33 +0900

    edited contact icons

--- src/static/contact.tsx
+++ src/static/contact.tsx
@@ -1,9 +1,9 @@
-import { GithubOutlined, LinkedinOutlined } from "@ant-design/icons";
+import { GithubOutlined, LinkedinOutlined, MailOutlined, PhoneOutlined } from "@ant-design/icons";
 import Link from "antd/lib/typography/Link";
 
 export default {
-    "email": "☏ +82 010-4478-6630",
-    "mobile": "✉ boleinikov@gmail.com",
+    "email": <div><PhoneOutlined /> +82 010-4478-6630</div>,
+    "mobile": <div><MailOutlined /> boleinikov@gmail.com</div>,
     "linkedin": <Link target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/boleinikov"><LinkedinOutlined /> linkedin.com/in/boleinikov</Link>,
     "github":  <Link target="_blank" rel="noopener noreferrer" href="https://github.com/bowdawn"><GithubOutlined /> github.com/bowdawn</Link>
 }
