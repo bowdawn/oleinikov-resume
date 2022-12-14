3c83d88fe35e224dcfd9eeb18cff5c912006a205
Oleinikov, Bogdan Konstantinovich <boleinikov@gmail.com>
2022-12-14 12:57:33 +0900

    fix txt

--- src/core/static/locales/kr/language.tsx
+++ src/core/static/locales/kr/language.tsx
@@ -1,7 +1,7 @@
 import Language from "../../../types/Language";
 
 export default {
-    "english" : "영어 (원어민/미국출신) ",
-    "korean" : "한국어 (유창함/경험 6년이상)",
-    "russian" : "러시아어 (유창함/계승어)",
-} as Language
+  english: "영어 (원어민/미국출신) ",
+  korean: "한국어 (유창함/경험 6년이상)",
+  russian: "러시아어 (유창함/모국어)",
+} as Language;
